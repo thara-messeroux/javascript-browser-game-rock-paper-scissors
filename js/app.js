32d055ffc062f838ef0d7ac776f914e2b9daeea8
thara-messeroux <tharamesseroux@gmail.com>
2025-12-15 14:12:07 -0500

    feat: add game state variables

--- js/app.js
+++ js/app.js
@@ -4,6 +4,13 @@ console.log('Hello World!');
 const choices = ['rock', 'paper', 'scissors'];
 
 /*-------------------------------- Variables --------------------------------*/
+let computerChoice; /* will store the computer’s current move */
+let msg; /* will store the message shown to the user (win/lose/tie) - the computer’s selection, which is randomly determined,*/
+let playerChoice; /* will store the player’s current move */
+
+/* For now, we’ll only initialize these variables and assign their values based on the game’s progression later.*/
+
+
 
 /*------------------------ Cached Element References ------------------------*/
 
