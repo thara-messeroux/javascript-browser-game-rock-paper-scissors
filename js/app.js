f8133e172335d6ff6ed93ec1fe3eff15b3a625f4
thara-messeroux <tharamesseroux@gmail.com>
2025-12-16 20:01:56 -0500

    feat: capture player choice from button click

--- js/app.js
+++ js/app.js
@@ -13,17 +13,29 @@ let playerChoice; /* will store the player’s current move */
 /* For now, we’ll only initialize these variables and assign their values based on the game’s progression later.*/
 
 
-
 /*------------------------ Cached Element References ------------------------*/
 
 const resultDisplayEl = document.querySelector('#result-display');
 
-
 /*-------------------------------- Functions --------------------------------*/
 
 /* Runs when a player clicks Rock, Paper, or Scissors */
+// const play = (event) => {
+    // console.log(event);            // everything
+//     console.log(event.target);     // the button
+    // console.log(event.target.id);  // "rock", "paper", or "scissors"
+
+// };
+
+/* Sets playerChoice based on which button was clicked */
+const getPlayerChoice = (event) => {
+    playerChoice = event.target.id;
+};
+
+/* Runs when a player clicks Rock, Paper, or Scissors */
 const play = (event) => {
-    console.log(event.target);
+    getPlayerChoice(event);
+    console.log(playerChoice);
 };
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -32,7 +44,7 @@ const play = (event) => {
 document.querySelector('#rock').addEventListener('click', play);
 document.querySelector('#paper').addEventListener('click', play);
 document.querySelector('#scissors').addEventListener('click', play);
- */
+*/
 
 document.querySelectorAll('button').forEach(function (button) {
     button.addEventListener('click', play);
