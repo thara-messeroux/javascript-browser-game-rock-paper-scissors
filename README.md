9653dce4af6468987e24d7ec278b287f9a6dea06
thara-messeroux <tharamesseroux@gmail.com>
2026-01-05 18:37:52 -0800

    read.me and scaffolding starting

---
+++ README.md
@@ -0,0 +1,19 @@
+// 1) Define any variables used to track the state of the game:
+//    The players choice
+//    The computers choice
+//    The match result - win/lose/tie
+//    A result message - display who won that round
+
+// 2) Define the required constants:
+//    There are only 3 choices a user can make ("rock", "paper", "scissors")
+//    We'll need a reference to a DOM element to display messages
+
+// 3) Handle a player clicking a button
+
+// 4) Handle generating random selections for the computer player
+
+// 5) Compare the player choice to the computer choice, and check for a winner
+
+// 6) Render a win/lose/tie message to the player 
+//    Include both player and computer selections in the message
+//    Clearly indicate who won
--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 console.log("Hello from app.js");
+
+/*-------------------------------- Constants --------------------------------*/
+const choices = ["rock", "paper", "scissors"];
+/*-------------------------------- Variables --------------------------------*/
+let playerChoice
+let computerChoice
+let msg
+/*------------------------ Cached Element References ------------------------*/
+const resultDisplay = document.querySelector('#result-display');
+/*-------------------------------- Functions --------------------------------*/
+const play = (event) => {   
+    console.log(event.target)
+    console.dir(event.target)
+}
+/*----------------------------- Event Listeners -----------------------------*/
+document.querySelector('main').addEventListener('click', play)
